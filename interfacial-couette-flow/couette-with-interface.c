e381ce8b4a7b6bfbe91240e8df37e777d17f53c0
svsanvesh <me20mtech14011@iith.ac.in>
2021-09-03 14:23:38 +0530

    upgrade

--- interfacial-couette-flow/couette-with-interface.c
+++ interfacial-couette-flow/couette-with-interface.c
@@ -5,7 +5,7 @@
 // Code comments: The code runs and provides the right results i.e.  the desired linear velocity profile.  
 // CONCLUSION: THE CODE WORKS FOR A 1X1 DOMAIN WITH BOUNDARY CONDITIONS GIVEN AS SO AND WE ARE ABLE TO PLOT A LINEAR VELOCITY PROFILE. 
 //
-//
+// for compiling : qcc -O2 couette-with-interface.c -L$BASILISK/gl -lglutils -lfb_osmesa -lGLU -lOSMesa -lm
 //
 //
 //// Libraries included
@@ -13,24 +13,27 @@
 #include "navier-stokes/centered.h"
 #include "vtk.h"
 #include "two-phase.h"
-#include "contact.h"
-#include "tension.h"
-
-
+//#include "contact.h"
+//#include "tension.h"
+#include "view.h"
 
 // Computational parameters
-double Reynolds = 20.0;       // Reynolds number
-int maxlevel = 7;              // Maximum mesh refinement
+double Reynolds = 5.0;       // Reynolds number
+int maxlevel = 6;              // Maximum mesh refinement
 face vector muv[];             // viscosity
 double H0;
 double U0;
 char name_vtk[100];
 
+/*
 double theta_bot = 90;
 double theta_top = 90;
 vector h[];
 h.t[bottom] = contact_angle (theta_bot*pi/180.);
 h.t[top] = contact_angle (theta_top*pi/180.);
+*/
+
+
 int main() {                // Main program begins here
 	L0 = 1.;            // Size of the square box
 	
@@ -38,13 +41,12 @@ int main() {                // Main program begins here
 	H0 = 0.2;            // Height of the channel
 	U0 =10.0;             // Velocity of the bottom plate
 	origin (-L0/2., -L0/2.0);  // Origin is at the bottom centre of the box
-	N = 128;
-//	mu = muv;           // constant viscosity. Exact value given below
+	N = 64;
 
 
-        f.sigma = 1.;
+/*        f.sigma = 1.;
 	f.height = h;
-
+*/
 	run();
 
 }
@@ -94,8 +96,8 @@ event logfile (i++)
 
 
 //THIS IS WHERE GFS FILES AARE GENERATED. 
-
-event snapshot (i += 1  ; t <=0.05) {
+/*
+event snapshot (i += 1  ; t <=0.4) {
   char name[80];
   sprintf (name, "snapshot-%d.gfs", i);
   scalar pid[];
@@ -104,17 +106,27 @@ event snapshot (i += 1  ; t <=0.05) {
   output_gfs (file = name);
   dump("t"); 
 }
+*/
+event mp(i += 1  ; t <=0.5) 
+{
 
+  clear();
+  draw_vof ("f");
+  begin_mirror ({0,-1});
+  cells();
+  end_mirror();
+
+  save ("movie.mp4");
+}	
+
+	
 // Produce paraviewable files 
-event movies (i += 1  ; t <=0.04)
+event movies (i += 1  ; t <=0.4)
 {
 	foreach()
-		
-			
-
-        sprintf (name_vtk, "data-%d.vtk", i);
-        FILE * fpvtk = fopen (name_vtk, "w");
-        output_vtk ({u.x,u.y,p,f},N,fpvtk,1);
+	       	sprintf (name_vtk, "data-%d.vtk", i);
+        	FILE * fpvtk = fopen (name_vtk, "w");
+        	output_vtk ({u.x,u.y,p,f},N,fpvtk,1);
 
 
 }
@@ -122,7 +134,16 @@ event movies (i += 1  ; t <=0.04)
   char name[80];
   dump (name, list = {f});
 } */
+
+
 // Using adaptive grid based on interface position
 event adapt (i++) {
-	adapt_wavelet ((scalar*){u}, (double[]){0.0001,0.0001}, 10);   
+	adapt_wavelet ((scalar*){f,u}, (double[]){0.1, 0.1,0.1}, 8);   
 }
+
+
+
+
+
+
+
