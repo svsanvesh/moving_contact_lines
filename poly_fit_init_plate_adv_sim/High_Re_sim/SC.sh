a1639e77eaa3aab86694fb2912a1ea590649ca27
svsanvesh <me20mtech14011@iith.ac.in>
2022-01-06 08:34:28 +0530

    serially run code in poly_fit/temp

--- poly_fit_init_plate_adv_sim/High_Re_sim/SC.sh
+++ poly_fit_init_plate_adv_sim/High_Re_sim/SC.sh
@@ -1,3 +1,3 @@
 touch *.*
-rm ./30d_plate_adv_air_water_r9_Re10_polyfit_init.tst
-CC='mpicc -D_MPI=4' make 30d_plate_adv_air_water_r9_Re10_polyfit_init.tst
+rm ./30d_plate_adv_air_water_r9_Re10_polyfit_init_long_run.tst
+CC='mpicc -D_MPI=4' make 30d_plate_adv_air_water_r9_Re10_polyfit_init_long_run.tst
